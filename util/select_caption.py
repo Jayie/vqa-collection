6b4dab7f8db2ee2f1cfff9b262b410046d66b0d2
Jayie <angie19941228@gmail.com>
2021-02-17 13:59:29 +0800

    Similarity metric between the caption and the Q-A pair

--- util/select_caption.py
+++ util/select_caption.py
@@ -11,20 +11,26 @@ def similarity(a, b):
     Output: the similarity score between a and b
     """
     output = 0
-    b_len = b.size(0)
+    a_len = a.size(0)
 
     # For all word w_a in a, find the word w_b in b that is most related to w_a,
     # and then sum up similarity(w_a, w_b)
-    for i in range(b_len):
+    for i in range(a_len):
         score = nn.functional.cosine_similarity(b, a[i,:].unsqueeze(0))
         score = torch.max(score).item()
         print(i, score)
         output += score
-    return output / b_len
+    return output / a_len
+
 
 def similarity_given_caption(question, answer, caption):
-    # TODO: compute the similarity between the caption and question-answer pair
-    return
+    """Input:
+        question: [q_len, embed_dim]
+        answer: [a_len, embed_dim]
+        caption: [c_len, embed_dim]
+    """
+    return (similarity(question, caption) + similarity(answer, caption)) / 2
+
 
 def select_caption():
     # TODO: select the relevant captions with the given Q-A pair.
