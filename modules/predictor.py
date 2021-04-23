96244a798ae4568afb4289f889f35be6f06cab5f
Jayie <angie19941228@gmail.com>
2021-04-23 16:20:03 +0800

    base caption predictor (using simple rnn to embed caption)

--- modules/predictor.py
+++ modules/predictor.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 
-from .modules import FCNet, CaptionEmbedding, LReLUNet
+from .modules import FCNet, CaptionEmbedding, LReLUNet, SentenceEmbedding
 from .attention import ConcatAttention
 
 def set_predictor(predictor_type: str,
@@ -26,6 +26,18 @@ def set_predictor(predictor_type: str,
             cls_layer=cls_layer,
             dropout=dropout
         )
+
+    if predictor_type == 'base-cap':
+        return BaseCaptionPredictor(
+            v_dim=v_dim,
+            embed_dim=embed_dim,
+            hidden_dim=hidden_dim,
+            ans_dim=ans_dim,
+            device=device,
+            cls_layer=cls_layer,
+            dropout=dropout
+        )
+    
     if predictor_type == 'q-cap':
         return PredictorwithCaption(
             embed_dim=embed_dim,
@@ -66,18 +78,62 @@ class BasePredictor(nn.Module):
         )
 
     def forward(self, batch):
-        batch['v'] = batch['v'].to(self.device)
-        batch['q'] = batch['q'].to(self.device)
+        v = batch['v'].to(self.device)
+        q = batch['q'].to(self.device)
 
-        v = batch['v'].sum(1) # [batch, v_dim]
+        v = v.sum(1) # [batch, v_dim]
 
         # FC layers
         v = self.v_net(v) # [batch, hidden_dim]
         
-        # Fuse visual and question features (multiplication here)
-        joint = batch['q'] * v # [batch, hidden_dim]
+        # Joint question features (multiply)
+        joint = q * v # [batch, hidden_dim]
         
         return self.classifier(joint)
+
+
+class BaseCaptionPredictor(BasePredictor):
+    def __init__(self,
+                 v_dim: int,
+                 embed_dim: int,
+                 hidden_dim: int,
+                 ans_dim: int,
+                 device: str,
+                 cls_layer: int = 2,
+                 dropout: float = 0.5,
+    ):
+        super().__init__(v_dim, hidden_dim, ans_dim, device, cls_layer, dropout)
+        self.c_rnn = SentenceEmbedding(
+            in_dim=embed_dim,
+            hidden_dim=hidden_dim,
+            rnn_layer=1,
+            device=device,
+            rnn_type='GRU'
+        )
+        self.c_net = FCNet(hidden_dim, hidden_dim)
+    
+    def forward(self, batch):
+        v = batch['v'].to(self.device)
+        q = batch['q'].to(self.device)
+        c = batch['c'].to(self.device)
+
+        # v_mean
+        v = v.sum(1) # [batch, v_dim]
+
+        # caption embedding
+        c = self.c_net(self.c_rnn(c))
+
+        # FC layers
+        v = self.v_net(v) # [batch, hidden_dim]
+
+        # Joint visual and caption embedding (concat)
+        joint = c + v
+        
+        # Joint question features (multiply)
+        joint = q * joint # [batch, hidden_dim]
+        
+        return self.classifier(joint)
+
 
 
 class PredictorwithCaption(nn.Module):
@@ -109,7 +165,7 @@ class PredictorwithCaption(nn.Module):
             device=device,
             dropout=dropout
         )
-        self.c_net = LReLUNet(embed_dim, hidden_dim, neg_slope)
+        self.c_net = LReLUNet(hidden_dim, hidden_dim, neg_slope)
         
         # For caption-attended visual features
         self.vq_net = LReLUNet(hidden_dim, hidden_dim, neg_slope)
